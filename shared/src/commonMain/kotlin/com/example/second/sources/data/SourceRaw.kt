c55d862be68850beeea12730636cf406ca0b025c
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2025-03-08 15:48:26 -0500

    worked on sources service

---
+++ shared/src/commonMain/kotlin/com/example/second/sources/data/SourceRaw.kt
@@ -0,0 +1,5 @@
+package com.example.second.sources.data
+
+class SourceRaw {
+
+}
---
+++ shared/src/commonMain/kotlin/com/example/second/sources/data/SourcesResponse.kt
@@ -0,0 +1,6 @@
+package com.example.second.sources.data
+
+class SourcesResponse {
+
+    val sources: List<SourceRaw>
+}
